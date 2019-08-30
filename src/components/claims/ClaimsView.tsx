5d6e85ce155d50c63e0a273b6154761892c2ebcb
jpda <john.dandison@live.com>
2019-08-30 14:06:15 -0400

    added toasts to ClaimsView

--- src/components/claims/ClaimsView.tsx
+++ src/components/claims/ClaimsView.tsx
@@ -22,31 +22,6 @@ export default class ClaimsView extends React.Component<Props, State> {
         this.data = props;
         this.auth = props.auth;
         this.state = { claims: [] };
-
-        if (props == null) {
-            console.warn("no claim data recived, using sample claim set");
-            var sampleClaimSet: any = {
-                "aud": "6e74172b-be56-4843-9ff4-e66a39bb12e3",
-                "iss": "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/v2.0",
-                "iat": 1537231048,
-                "nbf": 1537231048,
-                "exp": 1537234948,
-                "aio": "AXQAi/8IAAAAtAaZLo3ChMif6KOnttRB7eBq4/DccQzjcJGxPYy/C3jDaNGxXd6wNIIVGRghNRnwJ1lOcAnNZcjvkoyrFxCttv33140RioOFJ4bCCGVuoCag1uOTT22222gHwLPYQ/uf79QX+0KIijdrmp69RctzmQ==",
-                "azp": "6e74172b-be56-4843-9ff4-e66a39bb12e3",
-                "azpacr": "0",
-                "name": "Abe Lincoln",
-                "oid": "690222be-ff1a-4d56-abd1-7e4f7d38e474",
-                "preferred_username": "abeli@microsoft.com",
-                "rh": "I",
-                "scp": "access_as_user",
-                "sub": "HKZpfaHyWadeOouYlitjrI-KffTm222X5rrV3xDqfKQ",
-                "tid": "72f988bf-86f1-41af-91ab-2d7cd011db47",
-                "uti": "fqiBqXLPj0eQa82S-IYFAA",
-                "ver": "2.0"
-            };
-
-            this.parseToken(sampleClaimSet);
-        }
     }
 
     parseToken(token: any) {
@@ -72,8 +47,14 @@ export default class ClaimsView extends React.Component<Props, State> {
         console.error(e);
         console.error(e.errorCode);
         if (e.errorCode === "user_login_error") {
-            this.auth.msalObj.loginPopup(this.auth.data.requestConfig).then(token => { this.parseToken(token.idToken) });
+            this.auth.msalObj.loginPopup(this.auth.data.requestConfig).then(token => { this.parseToken(token.idToken) }).catch(e => { this.showError(e); });
+        } else {
+            this.showError(e);
         }
+    }
+
+    showError(e: any) {
+        this.props.toastToggle(true, e.errorCode);
     }
 
     render() {
